19bdbfdea5f85eebc502bdc3beeb1e271ea36b33
Mark Mandriota <mandriotamark@gmail.com>
2023-06-22 00:05:12 +0200

    fix integer constraint type

--- util.go
+++ util.go
@@ -21,7 +21,7 @@ import (
 
 type integer interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 func growSlice[E any](s []E, l int) []E {
